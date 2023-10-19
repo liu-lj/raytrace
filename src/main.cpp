f6224f16369d9622c95fe147fec753d3485ec949
llj <152836366+liu-lj@users.noreply.github.com>
2023-10-19 19:19:35 +0800

    调整参数，获得与 Image 17 一致的效果

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char **argv) {
   auto matGround = std::make_shared<Lambertian>(ColorF3(0.8, 0.8, 0.0));
   auto matCenter = std::make_shared<Lambertian>(ColorF3(0.1, 0.2, 0.5));
   auto matLeft = std::make_shared<Dielectric>(mfloat(1.5));
-  auto matRight = std::make_shared<Metal>(ColorF3(0.8, 0.6, 0.2), mfloat(1));
+  auto matRight = std::make_shared<Metal>(ColorF3(0.8, 0.6, 0.2), mfloat(0));
   scene.add(std::make_shared<Sphere>(100.0f, float3(0, -100.5, -1), matGround));
   scene.add(std::make_shared<Sphere>(0.5f, float3(0, 0, -1), matCenter));
   scene.add(std::make_shared<Sphere>(0.5f, float3(-1, 0, -1), matLeft));
